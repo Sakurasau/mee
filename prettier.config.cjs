95410ea1f1b66ddc3e74fc7d14271beb6dcd5fba
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-07-09 23:50:23 +0300

    chore: update prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,13 +3,14 @@
  * @type {import("prettier").Config}
  */
 
-export default {
+module.exports = {
+  tabWidth: 2,
   singleQuote: true,
   trailingComma: 'all',
   semi: false,
   bracketSpacing: true,
   bracketSameLine: true,
   endOfLine: 'auto',
-  tabWidth: 2,
+  useTabs: false,
   plugins: ['prettier-plugin-tailwindcss'],
-};
+}
