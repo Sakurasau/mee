ba423c3b8d6a44571031a2555afefa20079a22c6
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-30 01:08:36 +0200

    feat: enhance Vite configuration with tsconfig-paths integration

--- packages/mee-web/vite.config.ts
+++ packages/mee-web/vite.config.ts
@@ -1,22 +1,23 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
-import path, { resolve } from 'path'
-import dotenv from 'dotenv'
+// import path, { resolve } from 'path'
+// import dotenv from 'dotenv'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-dotenv.config({ path: resolve(__dirname, '../../.env') })
+// dotenv.config({ path: resolve(__dirname, '../../.env') })
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-  resolve: {
-    alias: {
-      '@mee': path.resolve(__dirname, '../mee-web/src'),
-      '@multi': path.resolve(__dirname, '../mee-multiplatform/src'),
-    },
-  },
+  plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
+  // resolve: {
+  //   alias: {
+  //     '@mee': path.resolve(__dirname, '../mee-web/src'),
+  //     '@multi': path.resolve(__dirname, '../mee-multiplatform/src'),
+  //   },
+  // },
 
-  base: process.env.VITE_CLIENT_WEB_BASE_PREFIX || '/',
+  base: process.env.VITE_CLIENT_WEB_BASE_PREFIX || '',
   server: {
     port: Number(process.env.VITE_CLIENT_WEB_PORT || 3000),
   },
