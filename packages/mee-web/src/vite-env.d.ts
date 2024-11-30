e8eb7f9757ab59d4f4206c26143e0e00ce83b3ca
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-30 21:28:29 +0200

    build: add env type (mee-web, vite)

--- packages/mee-web/src/vite-env.d.ts
+++ packages/mee-web/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+import { EnvSchemaType } from "@config/env/env.schema";
+
+type EnvSchema = z.infer<typeof schema>;
+
+declare global {
+  interface ImportMetaEnv extends EnvSchemaType {}
+}
