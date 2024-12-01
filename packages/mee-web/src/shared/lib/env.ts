03f5762813ca25a558495ee9d6327c61d8813384
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-12-01 11:20:46 +0200

    fix: isDev() & isProd()

--- packages/mee-web/src/shared/lib/env.ts
+++ packages/mee-web/src/shared/lib/env.ts
@@ -1,3 +1,3 @@
 
-export const isDev = () => import.meta.env.NODE_ENV !== 'production'
-export const isProd = () => import.meta.env.NODE_ENV === 'production'
+export const isDev = () => process.env.NODE_ENV !== 'production'
+export const isProd = () => process.env.NODE_ENV === 'production'
