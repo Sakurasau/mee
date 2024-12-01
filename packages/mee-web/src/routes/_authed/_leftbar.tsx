c3f8be454876997e19a5d2e182426adbf32571e3
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-12-01 02:52:54 +0200

    feat: base list chats 😍

--- packages/mee-web/src/routes/_authed/_leftbar.tsx
+++ packages/mee-web/src/routes/_authed/_leftbar.tsx
@@ -1,20 +1,59 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
-import { Splitter } from 'antd'
+import { Divider, Splitter } from 'antd'
 import { getChats, getRecommendationsChats } from '@mee/shared/api/chat'
+import Avatar from '@mee/shared/ui/Avatar'
+import { useEffect, useState } from 'react'
 
 export const Route = createFileRoute('/_authed/_leftbar')({
   component: LayoutComponent,
 })
 
 function LayoutComponent() {
+  const [chats, setChats] = useState([])
+  const [recommends, setRecommends] = useState([])
 
-  console.log('LayoutComponent')
-  getRecommendationsChats().then((chats) => { console.log('chats', chats) })
+  useEffect(() => {
+    getChats().then((chats) => {
+      chats.status === 200 && setChats(chats.data)
+      console.log('getChats', chats)
+    })
+    getRecommendationsChats().then((chats) => {
+      chats.status === 200 && setRecommends(chats.data)
+      console.log('getRecommendationsChats', chats)
+    })
+  }, [])
+
+  const Chat = ({ chat }: { chat: any }) => (
+    <div className="my-1 flex items-center gap-2 rounded-xl bg-slate-100 p-1">
+      <Avatar
+        id={chat.id}
+        title={chat.profile.display_name}
+        className="flex-none"
+      />
+      <p className="truncate">{chat.profile.display_name}</p>
+    </div>
+  )
+
+  const ContentLeftBar = () => (
+    <div>
+      <div className="flex gap-2">
+        {chats.map((chat) => (
+          <Chat key={chat.id} chat={chat} />
+        ))}
+      </div>
+      <Divider orientation="left" plain>
+        Recommends
+      </Divider>
+      {recommends.map((chat) => (
+        <Chat key={chat.id} chat={chat} />
+      ))}
+    </div>
+  )
 
   return (
     <Splitter className="h-screen">
       <Splitter.Panel defaultSize="20%" min="20%" max="60%">
-        <p>items</p>
+        <ContentLeftBar />
       </Splitter.Panel>
       <Splitter.Panel>
         <Outlet />
