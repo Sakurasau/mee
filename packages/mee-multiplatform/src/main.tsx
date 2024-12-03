7aaf6a44146661e869692874b63237fc72b38bcc
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-12-03 19:36:02 +0200

    feat: add custom font 'Rubik' to bundle (mee-multiplatform)

--- packages/mee-multiplatform/src/main.tsx
+++ packages/mee-multiplatform/src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from '@mee/main'
 
+import './styles/fonts.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />,
 )
