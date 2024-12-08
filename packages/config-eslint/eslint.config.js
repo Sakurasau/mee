edb0b2fc67fa9af4609e9394c6f4dde110b8cdfc
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-12-08 23:10:19 +0200

    feat: eslint "@config/tsconfig" & use

---
+++ packages/config-eslint/eslint.config.js
@@ -0,0 +1,72 @@
+import eslint from '@eslint/js'
+import tsParser from '@typescript-eslint/parser'
+import prettier from 'eslint-config-prettier'
+import _import from 'eslint-plugin-import'
+import jsdoc from 'eslint-plugin-jsdoc'
+import react from 'eslint-plugin-react'
+import tailwind from 'eslint-plugin-tailwindcss'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: ['**/build/*', '**/node_modules/*', '**/dist/*', '**/src-tauri/*'],
+  },
+  {
+    plugins: {
+      import: _import,
+      prettier: prettier,
+    },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parser: tsParser,
+    },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...tailwind.configs['flat/recommended'],
+  react.configs.flat.recommended,
+  // _import.flatConfigs.recommended,
+  jsdoc.configs['flat/recommended-typescript'],
+
+  // other ...
+  prettier,
+  {
+    // settings: {
+    //   tailwindcss: {
+    //     callees: ['className', 'clsx', 'cls', 'cva', 'cn'],
+    //   },
+    // },
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'type',
+            'builtin',
+            'object',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          pathGroups: [
+            {
+              pattern: '~/**',
+              group: 'external',
+              position: 'after',
+            },
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+])
